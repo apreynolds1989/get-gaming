ed2e9d2069d4d1eaffaf091e31c7158394619e2a
apreynolds1989 <70614076+apreynolds1989@users.noreply.github.com>
2022-06-11 18:13:43 -0230

    Added GetGamingLogo.svg to AppBar

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
-// import Logo from './img/GetGamingLogo.svg';
+import {ReactComponent as GetGamingLogo} from './img/GetGamingLogo.svg';
 
 const ResponsiveAppBar = () => {
     return (
         <AppBar position='fixed' color='transparent'>
             <Container maxWidth='xl'>
                 <Toolbar disableGutters>
-                    {/* <img src={Logo} alt='Get Gaming Logo' /> */}
+                    <GetGamingLogo />
                 </Toolbar>
             </Container>
         </AppBar>
