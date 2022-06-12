832b48acf33429cdc740fdbfff2665d821e420e8
apreynolds1989 <70614076+apreynolds1989@users.noreply.github.com>
2022-06-12 01:20:43 -0230

    Fixed Logo size, added Link Buttons without working links

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
-import {ReactComponent as GetGamingLogo} from './img/GetGamingLogo.svg';
+import { ReactComponent as GetGamingLogo } from './img/GetGamingLogo.svg';
+import { Box } from '@mui/system';
+import Button from '@mui/material/Button';
+
+const pages = ['Home', 'Games', 'Gallery', 'Contact'];
 
 const ResponsiveAppBar = () => {
     return (
@@ -10,6 +14,16 @@ const ResponsiveAppBar = () => {
             <Container maxWidth='xl'>
                 <Toolbar disableGutters>
                     <GetGamingLogo />
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'flex-end' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                sx={{ my: 2, color: '#6B3FA0', display: 'block', fontSize: 18 }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
